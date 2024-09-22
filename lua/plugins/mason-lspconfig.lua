2ee2a87f7190f71f2ac8fb956dd29bd432b20caf
yellowFloppyDrive <yellowfloppydrive@gmail.com>
2024-09-22 14:20:37 +0200

    Updated Mason installs

--- lua/plugins/mason-lspconfig.lua
+++ lua/plugins/mason-lspconfig.lua
@@ -9,7 +9,12 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "ts_ls" }
+        ensure_installed = {
+          "lua_ls",
+          "ts_ls",
+          "stylua",
+          "eslint_d",
+        }
       })
     end
   },
