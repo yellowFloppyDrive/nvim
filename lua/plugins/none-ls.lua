b2ceb593c7e256345fba6aca2879001022045763
yellowFloppyDrive <yellowfloppydrive@gmail.com>
2024-09-22 14:18:58 +0200

    none-ls formatting

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,19 @@
+return {
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.diagnostics.eslint_d,
+			},
+			dependencies = {
+				"nvimtools/none-ls-extras.nvim",
+			},
+		})
+
+		vim.keymap.set("n", "fm", vim.lsp.buf.format, {})
+	end,
+}
