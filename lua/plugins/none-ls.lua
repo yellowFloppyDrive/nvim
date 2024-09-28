6cef28b70cb9686d2e31459096da49571e817575
yellowFloppyDrive <yellowfloppydrive@gmail.com>
2024-09-28 20:35:49 +0200

    Reformatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,18 +1,18 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-			},
-			dependencies = {
-				"nvimtools/none-ls-extras.nvim",
-			},
-		})
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.prettier,
+      },
+      dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+      },
+    })
 
-		vim.keymap.set("n", "fm", vim.lsp.buf.format, {})
-	end,
+    vim.keymap.set("n", "fm", vim.lsp.buf.format, {})
+  end,
 }
